b4d6095e2aab5d3160ab293629468334444712bb
Adaptee <adaptee@gmail.com>
2010-12-07 09:24:43 +0800

    with the help of__getattr__(), the code is much shorter and cleaner now

--- cuesheet/cuesheet.py
+++ cuesheet/cuesheet.py
@@ -7,40 +7,37 @@ def get(table, key):
 
 class CueSheet(object):
 
+    supported_names = {
+                        "artist": "performer",
+                        "album": "title",
+                        "title": "title",
+                        "date" : "date",
+                        "genre": "genre",
+                        "comment": "comment",
+                        "file": "file",
+                        "tracks": "tracks",
+                        "flags": "flags",
+                        "discid": "discid",
+                        "songwriter": "songwriter",
+                        "cdtextfile": "cdtextfile",
+                      }
+
+    def __getattr__(self, key):
+        if key in self.supported_names:
+            value = self.table.get(self.supported_names[key], "")
+        else:
+            raise AttributeError("%s is not a supported attribute." % key)
+
+        setattr(self, key, value)
+        return value
+
     def __init__(self, table):
-
-        self._catalog    = table.get("catalog", "")
-        self._cdtextfile = table.get("cdtextfile", "")
-        self._file       = table.get("file", "")
-        self._flags      = table.get("flags", "")
-        self._performer  = table.get("performer", "")
-        self._songwriter = table.get("songwriter", "")
-        self._title      = table.get("title", "")
-        self._genre      = table.get("genre", "")
-        self._comment    = table.get("comment", "")
-        self._date       = table.get("date", "")
-        self._discid     = table.get("discid", "")
-
-        self.tracks     = table.get("tracks", "")
+        self.table = table
 
         for track in self.tracks:
             track.connect2cue(self)
 
-    def artist(self):
-        return self._performer
-
-    def album(self):
-        return self._title
-
-    def date(self):
-        return self._date
-
-    def genre(self):
-        return self._genre
-
-    def comment(self):
-        return self._comment
-
+    @property
     def tracktotal(self):
         return len(self.tracks)
 
@@ -54,9 +51,9 @@ class CueSheet(object):
 
         breakpoints = ""
 
-        # carefully skip the first track
+        # carefully, skip the first track
         for track in self.tracks[1:]:
-            breakpoints += "%s\n" % (track.offset(), )
+            breakpoints += "%s\n" % (track.offset, )
 
         return breakpoints
 
@@ -71,79 +68,55 @@ class CueSheet(object):
 
 class TrackInfo(object):
 
+    isolated_names = {
+                        "title"       : "title",
+                        "tracknumber" : "number",
+                        "offset"      : "offset01",
+                     }
+
+    connected_names = {
+                        "album"      : "album",
+                        "artist"     : "performer",
+                        "isrc"       : "isrc",
+                        "flags"      : "flags",
+                        "date"       : "date",
+                        "comment"    : "comment",
+                        "genre"      : "genre",
+                        "tracktotal" : "tracktotal",
+                        "songwriter" : "songwriter",
+                      }
+
+
+    def __getattr__(self, key):
+        if key in self.isolated_names:
+            value = self.table.get(self.isolated_names[key], "")
+
+        elif key in self.connected_names:
+            try:
+                value = self.table[key]
+            except KeyError:
+                value = getattr(self.cuesheet, key)
+        else:
+            raise AttributeError("%s is not a supported attribute." % key)
+
+        setattr(self, key, value)
+        return value
+
+
     def __init__(self, table):
-        self._title      = table.get("title", "")
-        self._performer  = table.get("performer", "")
-        self._number     = table.get("number", "")
-        self._offset     = table.get("offset01", "")
-        self._isrc       = table.get("isrc", "")
-        self._flags      = table.get("flags", "")
-        self._songwriter = table.get("songwriter", "")
-        self._date       = table.get("date", "")
-        self._comment    = table.get("comment", "")
-        self._genre      = table.get("genre", "")
-
-        self._artist = self._performer
-        self._album  = ""
-
+        self.table = table
         self.cuesheet = None
 
     def connect2cue(self, cuesheet):
         self.cuesheet = cuesheet
 
-    def title(self):
-        return self._title
-
-    def artist(self):
-        if not self._artist:
-            self._artist = self.cuesheet.artist()
-
-        return self._artist
-
-    def album(self):
-        if not self._album:
-            self._album = self.cuesheet.album()
-
-        return self._album
-
-    def date(self):
-        if not self._date:
-            self._date = self.cuesheet.date()
-
-        return self._date
-
-    def genre(self):
-        if not self._genre:
-            self._genre = self.cuesheet.genre()
-
-        return self._genre
-
-    def tracknumber(self):
-        return self._number
-
-    def tracktotal(self):
-        return self.cuesheet.tracktotal()
-
-    def comment(self):
-        if not self._comment:
-            self._comment = self.cuesheet.comment()
-
-        return self._comment
-
-    def offset(self):
-        return self._offset
-
     def debug_repr(self):
 
         result = ""
 
-        result += "title: %s, " % (self.title())
-        result += "artist: %s, " % (self.artist())
-        result += "tracknumber: %s, " % (self.tracknumber())
-        result += "offset: %s, " % (self.offset())
+        result += "title: %s, " % (self.title)
+        result += "artist: %s, " % (self.artist)
+        result += "tracknumber: %s, " % (self.tracknumber)
+        result += "offset: %s, " % (self.offset)
 
         return result
-
-
-
-
